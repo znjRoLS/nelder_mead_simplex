602eebe89d27e65981ee013644f438aa1630356d
rols <znjrols5@gmail.com>
2021-05-15 13:03:41 +0200

    uncommitted work

--- src/nelder_mead.cpp
+++ src/nelder_mead.cpp
@@ -1,2 +1,61 @@
 #include "nelder_mead.h"
 
+using Point = NelderMead::Point;
+using AlgorithmOptions = NelderMead::AlgorithmOptions;
+using ConvergenceOptions = NelderMead::ConvergenceOptions;
+
+NelderMead::NelderMead(function<double(const vector<double>&)> f, int dimension_opt):
+    _f(f),
+    _dimension(dimension),
+    _algo_opt({1, 2, 0.5, 0.5}),
+    _conv_opt({0, 0, 1e-7}) {
+}
+
+void NelderMead::SetAlgorithmOptions(const AlgorithmOptions& algo_opt) {
+  _algo_opt = algo_opt;
+}
+
+void NelderMead::SetConvergenceOptions(const ConvergenceOptions& conv_opt) {
+  _conv_opt = conv_opt;
+}
+
+void NelderMead::SetConstraints(vector<pair<double, double>> constraints) {
+  _constraints = constraints;
+}
+
+Point NelderMead::Run(vector<Point> x) {
+  while(!Iteration(x));
+  return x[0];
+}
+
+Point NelderMead::Run(Point x0) {
+  vector<Point> x = GetPoints(x0);
+  return Run(x);
+}
+
+Point NelderMead::Run() {
+  vector<Point> x = GetPointsUniform();
+  return Run(x);
+}
+
+vector<Point> NelderMead::GetPoints(Point x0) {
+  return vector<Point>();
+}
+
+vector<Point> NelderMead::GetPointsUniform() {
+  return vector<Point>();
+}
+
+vector<Point> NelderMead::GetPointsRandom() {
+  return vector<Point>();
+}
+
+bool NelderMead::Iteration(vector<Point> &points) {
+  sort(points.begin(), points.end(), [](const Point& lhs, const Point& rhs) {
+    return lhs.val < rhs.val;
+  });
+
+  vector<double>
+  Point x_mean
+
+}
--- src/nelder_mead.h
+++ src/nelder_mead.h
@@ -1,17 +1,17 @@
 #pragma once
 
 #include <vector>
+#include <function>
 
 using std::vector;
 using std::pair;
+using std::function;
 
 /// Implemented following this research paper:
 /// Gao, F. and Han, L. “Implementing the Nelder-Mead simplex algorithm with adaptive parameters”
 /// Computational Optimization and Applications, 51:1, 2012, pp. 259-277.
 class NelderMead {
-
  public:
-
   struct AlgorithmOptions {
     /// Reflection parameter
     double alfa;
@@ -38,23 +38,42 @@ class NelderMead {
     double val;
   };
 
-  NelderMead(const AlgorithmOptions& algo_opt, const ConvergenceOptions& conv_opt);
+  NelderMead(function<double(const vector<double>&)> f, int dimension);
+
+  void SetAlgorithmOptions(const AlgorithmOptions& algo_opt);
+  void SetConvergenceOptions(const ConvergenceOptions& conv_opt);
 
   /// Constraints are given for each dimension, as a vector (index being the dimension) of pairs of min and max values.
   /// If the constraint pair is {0,0}, it will be ignored.
   void SetConstraints(vector<pair<double,double>> constraints);
 
   /// Runing optimization, starting with simplex defined with _x.
-  vector<double> Run(vector<vector<double>> x);
+  /// These run functions are equivalent to:
+  ///   - Call GetPoints if needed
+  ///   - Run iteration until convergence is reached
+  Point Run(vector<Point> x);
   /// Runing optimization, will generate simplex based on one point.
-  vector<double> Run(vector<double> x0);
+  Point Run(Point x0);
   /// Runing optimization, will generate simplex uniformly.
-  vector<double> Run();
+  Point Run();
+
+  /// Generate points from a single one. Works the same as SciPy.
+  /// Generate a set of points from the original one by altering a single coordinate.
+  /// If a coord is 0, set it to 0.00025, and if not set coord to 1.05 of the original.
+  vector<Point> GetPoints(Point x0);
+
+  /// Generate points in the uniform manner accross the constraints. If a constriant is not set, (0,1) will be assumed.
+  vector<Point> GetPointsUniform();
+
+  /// Generate random points following constriants. If a constraint is not set, use (-inf, +inf).
+  vector<Point> GetPointsRandom();
 
   /// Perform a single iteration of this algorithm.
   bool Iteration(vector<Point>& points);
 
  private:
+  function<double(const vector<double>&)> _f;
+  int _dimension;
   AlgorithmOptions _algo_opt;
   ConvergenceOptions _conv_opt;
 
